14c79d74b0f24e4543251b935402be873c03ce33
Manoel Mateus <manoelmateus.borges@outlook.com>
2023-08-09 18:53:53 -0300

    adicionando funcionalidade de add todo e remover todo

--- javascript/main.js
+++ javascript/main.js
@@ -2,14 +2,26 @@ const inputTodo = document.querySelector('#input-add-todo');
 const todoList = document.querySelector('#todo-list');
 
 inputTodo.addEventListener("keypress", (e) => {
-    if(e.keyCode != "13"){
+    if(e.keyCode != "13" || e.target.value.trim() == ""){
        return;
     }
-    const addTodo = e.target;
+    const addTodoValue = e.target;
 
     const newTodoItem = document.createElement('div');
-    newTodoItem.textContent = 'teste';
+    newTodoItem.innerHTML = `
+    <p>${ addTodoValue.value }</p>
+    <button id="delete-todo">teste</button>`;
+
+    newTodoItem.querySelector("#delete-todo")
+    .addEventListener("click", deleteTodoitem)
+
     newTodoItem.classList.add('todo-item'); 
     todoList.appendChild(newTodoItem);
-    
+
+
+    inputTodo.value = '';
 })
+
+function deleteTodoitem(e){
+    e.target.parentElement.remove();
+}
